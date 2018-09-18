7b36945b48a73554f8304a159228bccd3ff2a95b
Torie Pham <tientp@uci.edu>
2018-09-17 17:00:14 -0700

    created a student grade table with React functional components

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,29 @@
+import React from 'react';
+import Table from './table';
 
+export default ()=>{
+    return (
+        <div className="container">
+            <h1>Student Grade Table</h1>
+            <Table data={students}/>
+        </div>
+    )
+}
 
+const students = [
+    {
+        student: 'Torie Pham',
+        course: 'Psychology',
+        grade: '85'
+    },
+    {
+        student:'Mia Tran',
+        course: 'Economics',
+        grade: '100'
+    },
+    {
+        student:'Timmy Token',
+        course: 'Anthropology',
+        grade: '90'
+    }
+];
---
+++ src/components/table.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default(props)=>{
+    const tableRows = props.data.map((item,index) => {
+        console.log(index);
+        return(
+            <tr key={index}>
+                <td>{item.student}</td>
+                <td>{item.course}</td>
+                <td>{item.grade}</td>
+            </tr>
+        )
+    });
+    return(
+        <table className="table">
+            <thead className="thead-inverse">
+                <tr>
+                    <th>Name</th>
+                    <th>Course</th>
+                    <th>Grade</th>
+                </tr>
+            </thead>
+            <tbody>
+                {tableRows}
+            </tbody>
+        </table>
+    )
+}
